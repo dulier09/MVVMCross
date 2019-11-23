457dfa6a8fae8110926fa9454394883ebd6e48cb
DULIER ARMANDO MU�OZ ZAPATA <duliermunoz163703@correo.itm.edu.co>
2019-11-22 22:50:37 -0500

    Core Finish

---
+++ TipCalculator.Core/Services/CalculationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalculator.Core.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        public decimal TipAmount(decimal subtotal, double generosity)
+        {
+            return subtotal * (decimal)(generosity / 100);
+        }
+    }
+}
---
+++ TipCalculator.Core/Services/ICalculationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalculator.Core.Services
+{
+    public interface ICalculationService
+    {
+        decimal TipAmount(decimal subtotal, double generosity);
+    }
+}
